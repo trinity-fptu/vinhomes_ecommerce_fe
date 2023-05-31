97cd184adaf1b7497393c0342392b3a24f28c295
thinh2002 <thinhndse16200@fpt.edu.vn>
2023-05-31 21:09:25 +0700

    fixed index

--- src/layouts/product/index.js
+++ src/layouts/product/index.js
@@ -12,46 +12,47 @@ import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
 import DataTable from "examples/Tables/DataTable";
 
-//data 
+// data
 import productData from "layouts/product/data/productData";
-function Products(){
-    const { columns, rows } = productData();
-    return (
-        <DashboardLayout>
-          <DashboardNavbar />
-          <MDBox pt={6} pb={3}>
-            <Grid container spacing={6}>
-              <Grid item xs={12}>
-                <Card>
-                  <MDBox
-                    mx={2}
-                    mt={-3}
-                    py={3}
-                    px={2}
-                    variant="gradient"
-                    bgColor="info"
-                    borderRadius="lg"
-                    coloredShadow="info"
-                  >
-                    <MDTypography variant="h6" color="white">
-                      Authors Table
-                    </MDTypography>
-                  </MDBox>
-                  <MDBox pt={3}>
-                    <DataTable
-                      table={{ columns, rows }}
-                      isSorted={false}
-                      entriesPerPage={false}
-                      showTotalEntries={false}
-                      noEndBorder
-                    />
-                  </MDBox>
-                </Card>
-              </Grid>
-            </Grid>
-          </MDBox>
-          <Footer />
-        </DashboardLayout>
-      );
-} 
+
+function Products() {
+  const { columns, rows } = productData();
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox pt={6} pb={3}>
+        <Grid container spacing={6}>
+          <Grid item xs={12}>
+            <Card>
+              <MDBox
+                mx={2}
+                mt={-3}
+                py={3}
+                px={2}
+                variant="gradient"
+                bgColor="info"
+                borderRadius="lg"
+                coloredShadow="info"
+              >
+                <MDTypography variant="h6" color="white">
+                  Authors Table
+                </MDTypography>
+              </MDBox>
+              <MDBox pt={3}>
+                <DataTable
+                  table={{ columns, rows }}
+                  isSorted={false}
+                  entriesPerPage={false}
+                  showTotalEntries={false}
+                  noEndBorder
+                />
+              </MDBox>
+            </Card>
+          </Grid>
+        </Grid>
+      </MDBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
 export default Products;
